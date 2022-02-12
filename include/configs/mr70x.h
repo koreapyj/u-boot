d1bcc7c0b24f4929ef4032c337c2d27c64d3acae
Yoonji Park <koreapyj@dcmys.kr>
2022-02-12 19:35:50 +0900

    test

---
+++ include/configs/mr70x.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2021 MediaTek Inc. All Rights Reserved.
+ *
+ * Author: Weijie Gao <weijie.gao@mediatek.com>
+ */
+
+#ifndef __CONFIG_MT7621_RFB_H
+#define __CONFIG_MT7621_RFB_H
+
+#include "mt7621.h"
+
+/* Network */
+#define CONFIG_IPADDR			192.168.1.1
+#define CONFIG_SERVERIP			192.168.1.100
+#define CONFIG_NETMASK			255.255.255.0
+
+#endif /* __CONFIG_MT7621_RFB_H */
